81e2eafee345a008dc3a9c97b0db44c50ee11b2c
BERKYT <BerkyT115@yandex.ru>
2024-03-16 18:41:54 +0300

    refactor

--- src/tgbot/services/utils/message_tools.py
+++ src/tgbot/services/utils/message_tools.py
@@ -18,7 +18,14 @@ def get_message(metadata: Message | CallbackQuery) -> Message:
         raise TypeError
 
 
-def send_status_task_smile(*, send_ok_status_smile: bool = True, lifetime_smile: float = 2.5):
+def send_status_task_smile(
+        *,
+        send_ok_status_smile: bool = True,
+        lifetime_smile: float = 2.5,
+        success_smile: str = "✅",
+        failed_smile: str = "❌",
+        waiting_smile: str = "⏳",
+):
     if not send_ok_status_smile:
         lifetime_smile = 0
 
@@ -28,7 +35,7 @@ def send_status_task_smile(*, send_ok_status_smile: bool = True, lifetime_smile:
                 metadata: Message | CallbackQuery, bot: AsyncTeleBot, *args, **kwargs
         ):
             message: Message = get_message(metadata)
-            msg_process: Message = await bot.send_message(message.chat.id, "⏳")
+            msg_process: Message = await bot.send_message(message.chat.id, waiting_smile)
 
             try:
                 if asyncio.iscoroutinefunction(func):
@@ -38,10 +45,10 @@ def send_status_task_smile(*, send_ok_status_smile: bool = True, lifetime_smile:
             except Exception as e:
                 LOGGER.err(e)
 
-                await bot.edit_message_text("❌", message.chat.id, msg_process.message_id)
+                await bot.edit_message_text(failed_smile, message.chat.id, msg_process.message_id)
             else:
                 if send_ok_status_smile:
-                    await bot.edit_message_text("✅", message.chat.id, msg_process.message_id)
+                    await bot.edit_message_text(success_smile, message.chat.id, msg_process.message_id)
                 return res
             finally:
                 await asyncio.sleep(lifetime_smile)
