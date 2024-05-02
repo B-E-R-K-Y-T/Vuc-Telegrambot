d659699bd5cee364a145d22c773123d696d54844
BERKYT <BerkyT115@yandex.ru>
2024-05-03 02:34:28 +0300

    added description to help command and text to reg command

--- src/tgbot/handlers/registration.py
+++ src/tgbot/handlers/registration.py
@@ -5,5 +5,5 @@ from telebot.types import Message
 async def reg_command_handler(message: Message, bot: AsyncTeleBot):
     await bot.send_message(
         message.chat.id,
-        "В настоящий момент регистрация отсутствует. " "Обратитесь к администратору",
+        "В настоящий момент регистрация отсутствует. Обратитесь к администратору",
     )
